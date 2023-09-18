81efecf2c201b5b4e43ccde613765f411166cd8f
Sazan1209 <Sazanovich.md@phystech.edu>
2023-09-18 16:39:47 +0300

    Basic main menu UI

---
+++ Game_Jam_project/Assets/src/View/LevelButton.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace src.View
+{
+    public class LevelButton : MonoBehaviour
+    {
+        public int levelNum;
+        public TMP_Text text;
+        void Start()
+        {
+            text.text = "Level " + levelNum;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
