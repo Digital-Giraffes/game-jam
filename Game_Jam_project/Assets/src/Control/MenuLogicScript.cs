b2844fd72c0d13c5df54abab1cf121b4a40c853e
Sazan1209 <Sazanovich.md@phystech.edu>
2023-09-20 19:41:55 +0300

    retry

--- Game_Jam_project/Assets/src/Control/MenuLogicScript.cs
+++ Game_Jam_project/Assets/src/Control/MenuLogicScript.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace src.Control
 {
@@ -45,7 +46,12 @@ namespace src.Control
 
     public void LoadLevel(int num)
     {
+      SceneManager.LoadScene(num);
+    }
 
+    public void Retry()
+    {
+      LoadLevel(SceneManager.GetActiveScene().buildIndex);
     }
   }
 }
