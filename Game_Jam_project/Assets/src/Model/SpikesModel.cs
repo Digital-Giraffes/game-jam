239c9d0b94c17bb83d4ad32db04c7df97fc0e69f
Kruzenger <kruzenger@gmail.com>
2023-09-23 11:28:06 +0300

    Revert "fixed lvls"
    This reverts commit 7349ed527b27cfb364ea4f99365393e461e14fc4.

--- Game_Jam_project/Assets/src/Model/SpikesModel.cs
+++ Game_Jam_project/Assets/src/Model/SpikesModel.cs
@@ -8,26 +8,24 @@ namespace model
     public float period;
     private PlayerModel _player_model;
     private AudioSource _spikes_sound;
+    private float _time = 0;
     private bool _active = false;
 
     private void Awake()
     {
       _player_model = GameObject.FindObjectOfType<PlayerModel>();
       _spikes_sound = gameObject.GetComponent<AudioSource>();
-      SetInactive();
     }
 
-    private void SetActive()
+    private void Update()
     {
-      _active = true;
-      Invoke("SetInactive", period);
-      _spikes_sound.Play();
-    }
-
-    private void SetInactive()
-    {
-      _active = false;
-      Invoke("SetActive", period);
+      _time += Time.deltaTime;
+      if (_time >= period)
+      {
+        _time = 0;
+        _spikes_sound.Play();
+        _active = !_active;
+      }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
