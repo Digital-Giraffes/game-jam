74a3532aa5c071bba111e7f37c031a469f73f9b6
Kruzenger <kruzenger@gmail.com>
2023-09-23 11:07:50 +0300

    Revert "fixing mr conflict"
    This reverts commit e3bef261056ea9bde344365577a66d7e90004a08.

---
+++ Game_Jam_project/Assets/src/Model/SpecialCoverModel.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace model
+{
+  public class SpecialCoverModel : MonoBehaviour
+  {
+    private AudioSource _carpet_sounds;
+    private PlayerModel _player_model;
+
+    private void Awake()
+    {
+      _carpet_sounds = gameObject.GetComponent<AudioSource>();
+      _player_model = GameObject.FindObjectOfType<PlayerModel>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+      _carpet_sounds.Play();
+      _player_model.SetCarpet(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+      _carpet_sounds.Pause();
+      _player_model.SetCarpet(false);
+    }
+  }
+}
